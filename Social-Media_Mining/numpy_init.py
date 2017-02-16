15446a56ccc24a40d190f325f71fa91c8ad50cc9
ankiyang <398753283@qq.com>
2017-02-16 08:57:08 +0800

    和豆浆一起学

---
+++ Social-Media_Mining/numpy_init.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3.5
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+data = [1,2,3]
+
+my_arr = np.array(data)
+
+
+if __name__ == '__main__':
+    print (my_arr)
+    print (my_arr.shape)
+    print (my_arr.dtype)
+    print (my_arr.ndim)
