854e9a6389e3ae4cdf601d2b04b92650d80476bb
ankiyang <398753283@qq.com>
2017-02-16 16:55:09 +0800

    social-mining - demo_sklearn-kmeans

---
+++ Social-Media_Mining/demo_sklearn.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3.5
+# -*-coding:utf-8-*-
+
+
+from sklearn import datasets
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+
+# Load the data
+iris = datasets.load_iris()
+X = iris.data
+
+petal_length = X[:,2]
+petal_width = X[:,3]
+true_labels = iris.target
+# print(true_labels)
+
+# Apply KMeans clustering
+estimator = KMeans(n_clusters=3)
+estimator.fit(X)
+predicted_labels = estimator.labels_
+
+# Color Scheme definition: red, yellow and blue
+color_scheme = ['r', 'y', 'b']
+
+# Markers definition: circle, "x" and "plus"
+marker_list = ['o', 'x', '+']
+
+# Assign colors/markers to the predicted labels
+colors_predicted_labels = [color_scheme[lab] for lab in predicted_labels]
+markers_predicted = [marker_list[lab] for lab in predicted_labels]
+
+# Plot and save the two scatter plots
+for x, y, c, m in zip(petal_length, petal_width, colors_predicted_labels, markers_predicted):
+    plt.scatter(x, y, c=c, marker=m)
+    plt.savefig('iris_true_labels.png')
+
+print(iris.target_names)
